0bbf9e0865dac49c2881cca4b56943a5042105ff
Apapora <47843562+Apapora@users.noreply.github.com>
2023-12-26 18:45:35 +0800

    removed policy output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,6 @@ output "thing_name" {
   value = aws_iot_thing.my_thing.name
 }
 
-output "policy_arn" {
-  value = aws_iot_policy.my_thing_policy[count.index].arn
-}
-
 output "device_certificate_pem" {
   value     = aws_iot_certificate.my_thing_cert.certificate_pem
   sensitive = true
